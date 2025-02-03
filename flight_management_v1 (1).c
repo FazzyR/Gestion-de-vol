a42cbe5cca53e3a77a7e92d0239364bd164da05d
Louay Djabali <louay2005123@gmail.com>
2025-02-03 01:03:29 +0100

    mainfunction

--- flight_management_v1 (1).c
+++ flight_management_v1 (1).c
@@ -124,16 +124,23 @@ void viewReservationHistory(Flight *head) {
 int main() {
     Flight *head = NULL;
     int choice, flightNumber, capacity;
+    char name[MAX_NAME_LENGTH];
 
     while (1) {
-        printf("\nFlight Management System Menu:\n");
+        printf("\nFlight Reservation System Menu:\n");
         printf("1. Add Flight\n");
-        printf("2. Exit\n");
+        printf("2. Add Reservation\n");
+        printf("3. Cancel Reservation\n");
+        printf("4. View Available Flights\n");
+        printf("5. View Reservation History\n");
+        printf("6. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
+        getchar();  
 
         switch (choice) {
             case 1:
+                // Adding a new flight
                 printf("Enter flight number: ");
                 scanf("%d", &flightNumber);
                 printf("Enter flight capacity: ");
@@ -143,11 +150,47 @@ int main() {
                     printf("Capacity must be greater than 0.\n");
                     break;
                 }
+
                 head = addFlight(head, flightNumber, capacity);
                 printf("Flight %d added with capacity %d.\n", flightNumber, capacity);
                 break;
 
             case 2:
+                printf("Enter flight number: ");
+                scanf("%d", &flightNumber);
+                getchar();  
+                printf("Enter passenger name: ");
+                fgets(name, sizeof(name), stdin);
+                name[strcspn(name, "\n")] = '\0'; 
+
+                if (!isValidName(name)) {
+                    printf("Invalid name. It must be non-empty and less than %d characters.\n", MAX_NAME_LENGTH);
+                    break;
+                }
+
+                addReservation(head, name, flightNumber);
+                break;
+
+            case 3:
+                printf("Enter flight number: ");
+                scanf("%d", &flightNumber);
+                getchar();  
+                printf("Enter passenger name: ");
+                fgets(name, sizeof(name), stdin);
+                name[strcspn(name, "\n")] = '\0'; 
+
+                cancelReservation(head, name, flightNumber);
+                break;
+
+            case 4:
+                searchAvailableFlights(head);
+                break;
+
+            case 5:
+                viewReservationHistory(head);
+                break;
+
+            case 6:
                 freeAll(head);
                 printf("Exiting the program.\n");
                 return 0;
@@ -157,3 +200,4 @@ int main() {
         }
     }
 }
+
